e5e80ac7c0d9e6c058fdcfc631d37c6de188d37c
Gabriel Bueno <gabrielhortmann@gmail.com>
2024-06-09 13:18:04 -0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from models.db import db, instance
-from models.auth.user import User
-
-from models.iot.sensor import Sensor
-from models.iot.parametro import Parametro
-from models.iot.alarme import Alarme
